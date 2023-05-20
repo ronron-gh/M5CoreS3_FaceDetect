90e9f1d1f6919a111bb88e28eb659fcce621b9b9
ronron-gh <motohiro.sanpei@gmail.com>
2023-05-20 09:44:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# M5CoreS3_CameraTest
+M5Stack CoreS3の内蔵カメラでキャプチャした画像をLCDに表示するテストプログラム。
+
+[mongonta0716](https://github.com/mongonta0716)さんの[M5CoreS3_HelloWorld](https://github.com/mongonta0716/M5CoreS3_HelloWorld/tree/master)をベースにしています。
+
+## 開発環境
+VSCode + PlatformIO
+
+## ライブラリ
+* M5Unified
+* esp32-camera
---
+++ src/main.cpp
@@ -0,0 +1,207 @@
+#include <Arduino.h>
+
+#include <M5Unified.h>
+#include <esp_camera.h>
+#include <fb_gfx.h>
+
+#include <vector>
+#include "human_face_detect_msr01.hpp"
+#include "human_face_detect_mnp01.hpp"
+
+#define TWO_STAGE 1 /*<! 1: detect by two-stage which is more accurate but slower(with keypoints). */
+                    /*<! 0: detect by one-stage which is less accurate but faster(without keypoints). */
+
+
+#define LCD_WIDTH (320)
+#define LCD_HEIGHT  (240)
+#define LCD_BUF_SIZE (LCD_WIDTH*LCD_HEIGHT*2)
+
+
+
+static camera_config_t camera_config = {
+    .pin_pwdn     = -1,
+    .pin_reset    = -1,
+    .pin_xclk     = 2,
+    .pin_sscb_sda = 12,
+    .pin_sscb_scl = 11,
+
+    .pin_d7 = 47,
+    .pin_d6 = 48,
+    .pin_d5 = 16,
+    .pin_d4 = 15,
+    .pin_d3 = 42,
+    .pin_d2 = 41,
+    .pin_d1 = 40,
+    .pin_d0 = 39,
+
+    .pin_vsync = 46,
+    .pin_href  = 38,
+    .pin_pclk  = 45,
+
+    .xclk_freq_hz = 20000000,
+    .ledc_timer   = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_RGB565,
+    //.pixel_format = PIXFORMAT_JPEG,
+    .frame_size   = FRAMESIZE_QVGA,   // QVGA(320x240)
+    .jpeg_quality = 0,
+    .fb_count     = 2,
+    .fb_location  = CAMERA_FB_IN_PSRAM,
+    .grab_mode    = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+esp_err_t camera_init(){
+
+    //initialize the camera
+    M5.In_I2C.release();
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        //Serial.println("Camera Init Failed");
+        M5.Display.println("Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+
+
+#define FACE_COLOR_WHITE  0x00FFFFFF
+#define FACE_COLOR_BLACK  0x00000000
+#define FACE_COLOR_RED    0x000000FF
+#define FACE_COLOR_GREEN  0x0000FF00
+#define FACE_COLOR_BLUE   0x00FF0000
+#define FACE_COLOR_YELLOW (FACE_COLOR_RED | FACE_COLOR_GREEN)
+#define FACE_COLOR_CYAN   (FACE_COLOR_BLUE | FACE_COLOR_GREEN)
+#define FACE_COLOR_PURPLE (FACE_COLOR_BLUE | FACE_COLOR_RED)
+
+static void draw_face_boxes(fb_data_t *fb, std::list<dl::detect::result_t> *results, int face_id)
+{
+    int x, y, w, h;
+    uint32_t color = FACE_COLOR_YELLOW;
+    if (face_id < 0)
+    {
+        color = FACE_COLOR_RED;
+    }
+    else if (face_id > 0)
+    {
+        color = FACE_COLOR_GREEN;
+    }
+    if(fb->bytes_per_pixel == 2){
+        //color = ((color >> 8) & 0xF800) | ((color >> 3) & 0x07E0) | (color & 0x001F);
+        color = ((color >> 16) & 0x001F) | ((color >> 3) & 0x07E0) | ((color << 8) & 0xF800);
+    }
+    int i = 0;
+    for (std::list<dl::detect::result_t>::iterator prediction = results->begin(); prediction != results->end(); prediction++, i++)
+    {
+        // rectangle box
+        x = (int)prediction->box[0];
+        y = (int)prediction->box[1];
+        w = (int)prediction->box[2] - x + 1;
+        h = (int)prediction->box[3] - y + 1;
+        if((x + w) > fb->width){
+            w = fb->width - x;
+        }
+        if((y + h) > fb->height){
+            h = fb->height - y;
+        }
+
+        //fb_gfx_fillRect(fb, x+10, y+10, w-20, h-20, FACE_COLOR_RED);  //モザイク
+
+        fb_gfx_drawFastHLine(fb, x, y, w, color);
+        fb_gfx_drawFastHLine(fb, x, y + h - 1, w, color);
+        fb_gfx_drawFastVLine(fb, x, y, h, color);
+        fb_gfx_drawFastVLine(fb, x + w - 1, y, h, color);
+
+
+
+#if TWO_STAGE
+        // landmarks (left eye, mouth left, nose, right eye, mouth right)
+        int x0, y0, j;
+        for (j = 0; j < 10; j+=2) {
+            x0 = (int)prediction->keypoint[j];
+            y0 = (int)prediction->keypoint[j+1];
+            fb_gfx_fillRect(fb, x0, y0, 3, 3, color);
+        }
+#endif
+    }
+}
+
+
+esp_err_t camera_capture_and_face_detect(){
+  //acquire a frame
+  M5.In_I2C.release();
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    //Serial.println("Camera Capture Failed");
+    M5.Display.println("Camera Capture Failed");
+    return ESP_FAIL;
+  }
+
+  int face_id = 0;
+
+#if TWO_STAGE
+  HumanFaceDetectMSR01 s1(0.1F, 0.5F, 10, 0.2F);
+  HumanFaceDetectMNP01 s2(0.5F, 0.3F, 5);
+  std::list<dl::detect::result_t> &candidates = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+  std::list<dl::detect::result_t> &results = s2.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3}, candidates);
+#else
+  HumanFaceDetectMSR01 s1(0.3F, 0.5F, 10, 0.2F);
+  std::list<dl::detect::result_t> &results = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+#endif
+  if (results.size() > 0) {
+      fb_data_t rfb;
+      rfb.width = fb->width;
+      rfb.height = fb->height;
+      rfb.data = fb->buf;
+      rfb.bytes_per_pixel = 2;
+      rfb.format = FB_RGB565;
+
+      draw_face_boxes(&rfb, &results, face_id);
+  }
+
+  //replace this with your own function
+  //process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+  M5.Display.startWrite();
+  M5.Display.setAddrWindow(0, 0, LCD_WIDTH, LCD_HEIGHT);
+  M5.Display.writePixels((uint16_t*)fb->buf, int(fb->len / 2));
+  M5.Display.endWrite();
+  
+  //return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
+
+  return ESP_OK;
+}
+
+
+void setup() {
+  auto cfg = M5.config();
+  cfg.output_power = true;
+  M5.begin(cfg);
+
+  M5.Display.setFont(&fonts::efontJA_24);
+  M5.Display.println("HelloWorld");
+
+  camera_init();
+
+}
+
+void loop() {
+  //Serial.println("Hello World");  // Print text on the serial port.在串口输出文本
+
+#if 0   // Check free size of heap memory
+  Serial.printf("===============================================================\n");
+  Serial.printf("Mem Test\n");
+  Serial.printf("===============================================================\n");
+  Serial.printf("esp_get_free_heap_size()                              : %6d\n", esp_get_free_heap_size() );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_DMA)               : %6d\n", heap_caps_get_free_size(MALLOC_CAP_DMA) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_SPIRAM)            : %6d\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_INTERNAL)          : %6d\n", heap_caps_get_free_size(MALLOC_CAP_INTERNAL) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_DEFAULT)           : %6d\n", heap_caps_get_free_size(MALLOC_CAP_DEFAULT) );
+  delay(1000);          // Delay [ms]
+#endif
+
+  camera_capture_and_face_detect();
+
+}
